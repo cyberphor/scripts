4e424d941b937485011f012afc87c1b18a325a4a
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2020-06-14 21:14:09 +0300

    Update Enable-Auditing.ps1

--- Enable-Auditing.ps1
+++ Enable-Auditing.ps1
@@ -1,3 +1,7 @@
+Param(
+    [switch]$Domain
+)
+
 function Audit-SexySix {
     $Auditpol = 'C:\Windows\System32\auditpol.exe'
     if ($Auditpol) {
@@ -23,5 +27,48 @@ function Audit-CliUsage {
     }
 }
 
-Audit-SexySix
-Audit-CliUsage
+<# CONTINUE HERE
+
+# 
+
+<?xml version="1.0" encoding="UTF-8"?>
+<Subscription xmlns="http://schemas.microsoft.com/2006/03/windows/events/subscription">
+        <SubscriptionId>SexySix</SubscriptionId>
+        <SubscriptionType>SourceInitiated</SubscriptionType>
+        <Description></Description>
+        <Enabled>true</Enabled>
+        <Uri>http://schemas.microsoft.com/wbem/wsman/1/windows/EventLog</Uri>
+        <ConfigurationMode>Normal</ConfigurationMode>
+        <Delivery Mode="Push">
+                <Batching>
+                        <MaxLatencyTime>900000</MaxLatencyTime>
+                </Batching>
+                <PushSettings>
+                        <Heartbeat Interval="900000"/>
+                </PushSettings>
+        </Delivery>
+        <Query>
+                <![CDATA[
+<QueryList><Query Id="0"><Select Path="Application">*[System[(EventID=4688 or EventID=4624 or EventID=5140 or EventID=4663 or EventID=4657 or EventID=5156)]]</Select><Select Path="Security">*[System[(EventID=4688 or EventID=4624 or EventID=5140 or EventID=4663 or EventID=4657 or EventID=5156)]]</Select><Select Path="Setup">*[System[(EventID=4688 or EventID=4624 or EventID=5140 or EventID=4663 or EventID=4657 or EventID=5156)]]</Select><Select Path="System">*[System[(EventID=4688 or EventID=4624 or EventID=5140 or EventID=4663 or EventID=4657 or EventID=5156)]]</Select><Select Path="Microsoft-Windows-PowerShell/Operational">*[System[(EventID=4688 or EventID=4624 or EventID=5140 or EventID=4663 or EventID=4657 or EventID=5156)]]</Select></Query></QueryList>
+                ]]>
+        </Query>
+        <ReadExistingEvents>false</ReadExistingEvents>
+        <TransportName>HTTP</TransportName>
+        <ContentFormat>RenderedText</ContentFormat>
+        <Locale Language="en-US"/>
+        <LogFile>ForwardedEvents</LogFile>
+        <PublisherName>Microsoft-Windows-EventCollector</PublisherName>
+        <AllowedSourceNonDomainComputers>
+                <AllowedIssuerCAList>
+                </AllowedIssuerCAList>
+        </AllowedSourceNonDomainComputers>
+        <AllowedSourceDomainComputers>O:NSG:BAD:P(A;;GA;;;DC)S:</AllowedSourceDomainComputers>
+</Subscription>
+#>
+
+if ($Domain) {
+    # domain stuff
+} else {
+    Audit-SexySix
+    Audit-CliUsage
+}
