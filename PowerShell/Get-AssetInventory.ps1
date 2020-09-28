be4286d5708a0eb6757762033b7abc35c97188d7
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2020-09-28 07:53:10 -0600

    Create Get-AssetInventory.ps1

---
+++ PowerShell/Get-AssetInventory.ps1
@@ -0,0 +1,37 @@
+Param(
+    $ComputerName
+)
+
+function Get-AssetInventory {
+    $ScriptBlock = {
+        Param($Target)
+        Write-Host "[+] $Target, MAC Address, Hostname, Serial Number, User"
+    }
+
+    $Targets | ForEach-Object {
+        Start-Job -ScriptBlock $ScriptBlock -ArgumentList $_ | Out-Null
+    }
+
+    Get-Job | Wait-Job | Receive-Job
+}
+
+function Main {
+    if (($ComputerName.GetType().Name -eq 'String') -and (Test-Path $ComputerName)) {
+        $Targets = Get-Content $ComputerName
+    } elseif ($ComputerName.GetType().Name -eq 'Object[]') {
+        $Targets = $ComputerName  
+    } elseif ($ComputerName.GetType().Name -eq 'String') {
+        $Targets = $ComputerName
+    } else {
+        Write-Host "[x] No machines specified."
+        exit
+    }
+
+    Get-AssetInventory
+}
+
+Main
+
+# REFERENCES
+# https://stackoverflow.com/questions/16360019/how-do-i-add-multi-threading
+# https://stackoverflow.com/questions/15120597/passing-multiple-values-to-a-single-powershell-script-parameter
