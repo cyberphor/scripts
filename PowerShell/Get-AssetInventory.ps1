faa6c1a47535b21e986fa215670c74d376d0d2f5
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2020-09-29 07:59:28 -0600

    Update Get-AssetInventory.ps1

--- PowerShell/Get-AssetInventory.ps1
+++ PowerShell/Get-AssetInventory.ps1
@@ -3,31 +3,65 @@ Param(
 )
 
 function Get-AssetInventory {
-    $ScriptBlock = {
-        Param($Target)
-        Write-Host "[+] $Target, MAC Address, Hostname, Serial Number, User"
+    $Baseline = Test-Path $ComputerName
+    $NotList = $ComputerName.GetType().Name -eq 'String'
+    $Assets = @()
+
+    if ($Baseline -and $NotList) {
+        Get-Content $ComputerName |
+        ForEach-Object {
+            if ("$_" -as [IPAddress] -as [Bool]) {
+                $Online = Test-Connection -Count 2 -ComputerName $_ -Quiet
+                $IpAddress = $_
+                $MacAddress = ''
+                $Hostname = ''
+                $SerialNumber = ''
+                $CurrentUser = ''
+            } else {
+                $Online = Test-Connection -Count 2 -ComputerName $_ -Quiet
+                $IpAddress = $Online.IPV4Address | Select -Last 1 -ExpandProperty IPAddressToString
+                $MacAddress = ''
+                $Hostname = $_
+                $SerialNumber = ''
+                $CurrentUser = ''
+            }
+
+            $Asset = New-Object -TypeName PSObject
+            Add-Member -InputObject $Asset -MemberType NoteProperty -Name Online -Value $Online 
+            Add-Member -InputObject $Asset -MemberType NoteProperty -Name IpAddress -Value $IpAddress
+            Add-Member -InputObject $Asset -MemberType NoteProperty -Name MacAddress -Value $MacAddress
+            Add-Member -InputObject $Asset -MemberType NoteProperty -Name Hostname -Value $Hostname
+            Add-Member -InputObject $Asset -MemberType NoteProperty -Name SerialNumber -Value $SerialNumber
+            Add-Member -InputObject $Asset -MemberType NoteProperty -Name CurrentUser -Value $CurrentUser
+            $Assets += $Asset
+
+            }
+    } else {
+        $Targets = $ComputerName
     }
 
-    $Targets | ForEach-Object {
-        Start-Job -ScriptBlock $ScriptBlock -ArgumentList $_ | Out-Null
+    $Assets | ForEach-Object {
+        $Online = $_.Online
+        $IpAddress = $_.IpAddress
+        $MacAddress = $_.MacAddress
+        $Hostname = $_.Hostname
+        $SerialNumber = $_.SerialNumber
+        $CurrentUser = $_.CurrentUser 
+        if ($Online) {
+            Write-Host "[+] $IpAddress, $MacAddress, $Hostname, $SerialNumber, $CurrentUser"
+        } else {
+            Write-Host "[x] $IpAddress, $MacAddress, $Hostname, $SerialNumber, $CurrentUser"
+        }
     }
-
-    Get-Job | Wait-Job | Receive-Job
 }
 
 function Main {
-    if (($ComputerName.GetType().Name -eq 'String') -and (Test-Path $ComputerName)) {
-        $Targets = Get-Content $ComputerName
-    } elseif ($ComputerName.GetType().Name -eq 'Object[]') {
-        $Targets = $ComputerName  
-    } elseif ($ComputerName.GetType().Name -eq 'String') {
-        $Targets = $ComputerName
-    } else {
+    if ($ComputerName) {
+        Get-AssetInventory 
+    } else { 
         Write-Host "[x] No machines specified."
         exit
     }
-
-    Get-AssetInventory
 }
 
 Main
@@ -37,3 +71,4 @@ Main
 # https://stackoverflow.com/questions/15120597/passing-multiple-values-to-a-single-powershell-script-parameter
 # https://stackoverflow.com/questions/13264369/how-to-pass-array-of-arguments-to-powershell-commandline
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/start-job?view=powershell-7
+# https://ridicurious.com/2018/11/14/4-ways-to-validate-ipaddress-in-powershell/
