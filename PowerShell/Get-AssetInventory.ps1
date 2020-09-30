b5acbfd1d18c1737f0c1d086a01ce4b73f228306
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2020-09-30 06:09:25 -0600

    Update Get-AssetInventory.ps1

--- PowerShell/Get-AssetInventory.ps1
+++ PowerShell/Get-AssetInventory.ps1
@@ -3,39 +3,46 @@ Param(
 )
 
 function Get-AssetInventory {
-    $Baseline = Test-Path $ComputerName
-    $NotList = $ComputerName.GetType().Name -eq 'String'
     $Assets = @()
-
-    if ($Baseline -and $NotList) {
+    if (Test-Path $ComputerName) {
         Get-Content $ComputerName |
         ForEach-Object {
-            if ("$_" -as [IPAddress] -as [Bool]) {
-                $Online = Test-Connection -Count 2 -ComputerName $_ -Quiet
-                $IpAddress = $_
-                $MacAddress = ''
-                $Hostname = ''
-                $SerialNumber = ''
-                $CurrentUser = ''
-            } else {
-                $Online = Test-Connection -Count 2 -ComputerName $_ -Quiet
-                $IpAddress = $Online.IPV4Address | Select -Last 1 -ExpandProperty IPAddressToString
-                $MacAddress = ''
-                $Hostname = $_
-                $SerialNumber = ''
-                $CurrentUser = ''
-            }
+            $Online = Test-Connection -Count 2 -ComputerName $_ -ErrorAction SilentlyContinue
+            if ($Online) {
+                if ("$_" -as [IPAddress] -as [Bool]) {
+                    # ip address
+                    $IpAddress = $_
 
-            $Asset = New-Object -TypeName PSObject
-            Add-Member -InputObject $Asset -MemberType NoteProperty -Name Online -Value $Online 
-            Add-Member -InputObject $Asset -MemberType NoteProperty -Name IpAddress -Value $IpAddress
-            Add-Member -InputObject $Asset -MemberType NoteProperty -Name MacAddress -Value $MacAddress
-            Add-Member -InputObject $Asset -MemberType NoteProperty -Name Hostname -Value $Hostname
-            Add-Member -InputObject $Asset -MemberType NoteProperty -Name SerialNumber -Value $SerialNumber
-            Add-Member -InputObject $Asset -MemberType NoteProperty -Name CurrentUser -Value $CurrentUser
-            $Assets += $Asset
+                    # mac address
+                    $ArpCache = Get-NetNeighbor | 
+                    Where-Object { $_.IPAddress -eq "$IpAddress"} | 
+                    Select -ExpandProperty LinkLayerAddress 
+                    $MacAddress = ([regex]::split("$ArpCache", '(.{2})') | 
+                    Where-Object { $_ }) -join '-'
 
-            }
+                    # os
+                    $OS = $Online | Select -ExpandProperty TimeToLive
+
+                    # hostname
+                    $Hostname = ''
+
+                    # serial number
+                    $SerialNumber = ''
+
+                    # user
+                    $CurrentUser = ''
+                }
+            
+                $Asset = New-Object -TypeName PSObject
+                Add-Member -InputObject $Asset -MemberType NoteProperty -Name Online -Value $Online 
+                Add-Member -InputObject $Asset -MemberType NoteProperty -Name IpAddress -Value $IpAddress
+                Add-Member -InputObject $Asset -MemberType NoteProperty -Name MacAddress -Value $MacAddress
+                Add-Member -InputObject $Asset -MemberType NoteProperty -Name Hostname -Value $Hostname
+                Add-Member -InputObject $Asset -MemberType NoteProperty -Name SerialNumber -Value $SerialNumber
+                Add-Member -InputObject $Asset -MemberType NoteProperty -Name CurrentUser -Value $CurrentUser
+                $Assets += $Asset
+            } 
+        }
     } else {
         $Targets = $ComputerName
     }
@@ -72,3 +79,5 @@ Main
 # https://stackoverflow.com/questions/13264369/how-to-pass-array-of-arguments-to-powershell-commandline
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/start-job?view=powershell-7
 # https://ridicurious.com/2018/11/14/4-ways-to-validate-ipaddress-in-powershell/
+# https://sqljana.wordpress.com/2015/08/25/get-operating-system-name-linux-unix-windows-using-powershell/
+# https://docs.microsoft.com/en-us/powershell/module/nettcpip/get-netneighbor?view=win10-ps
