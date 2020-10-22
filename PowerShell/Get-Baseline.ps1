5912f9fd9a57d2e6dca2a292321e2961b16ebf89
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2020-10-22 06:50:12 -0600

    Update Get-Baseline.ps1

--- PowerShell/Get-Baseline.ps1
+++ PowerShell/Get-Baseline.ps1
@@ -1,3 +1,17 @@
-# Accounts
+# ACCOUNTS: Users
 Get-WmiObject -Class Win32_UserAccount | Select -ExpandProperty Name
-net localgroup Administrators
+
+# ACCOUNTS: Local Administrators
+net localgroup administrators | Where-Object { $_ -and $_ -notmatch "The command completed successfully." } | Select -Skip 4
+
+# SMB USAGE: Local shares
+Get-SmbShare | Select Name, Path | Sort-Object -Property Path | Format-Table -AutoSize
+
+# SMB USAGE: Inbound SMB Sessions
+Get-SmbSession
+
+# SMB USAGE: Outbound SMB Sessions
+net use
+
+# NETWORK USAGE: TCP Ports
+Get-NetTCPConnection
