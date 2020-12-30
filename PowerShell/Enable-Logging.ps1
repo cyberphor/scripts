6c35d432ae0f9379d55202d362e5d9006848819c
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2020-12-29 19:25:02 -0700

    Update Enable-Logging.ps1

--- PowerShell/Enable-Logging.ps1
+++ PowerShell/Enable-Logging.ps1
@@ -1,62 +1,100 @@
-Param(
-    [switch]$CommandLine,
-    [switch]$FileShare,
-    [switch]$FileSystem,
-    [switch]$Firewall,
-    [switch]$Logon,
-    [switch]$ProcessCreation,
-    [switch]$Registry
+<#
+.SYNOPSIS
+    Enables logging.
+.EXAMPLE
+    ./Enable-Logging.ps1 -Show logon,firewall
+    ./Enable-Logging.ps1 -Show logon,firewall,'process creation'
+    ./Enable-Logging.ps1 -Show process
+    ./Enable-Logging.ps1 -Set default
+    ./Enable-Logging.ps1 -Set logon -Value success
+    ./Enable-Logging.ps1 -Set firewall -Value success,failure
+.INPUTS
+    None.
+.OUTPUTS
+    None.
+.LINK
+    https://www.github.com/cyberphor/scripts/PowerShell/Enable-Logging.ps1
+.NOTES
+    File name: Enabling-Logging.ps1
+    Version: 2.0
+    Author: Victor Fernandez III
+    Creation Date: Tuesday, December 29, 2020
+.REFERENCES
+    https://stackoverflow.com/questions/5648931/test-if-registry-value-exists
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_parameter_sets?view=powershell-7.1
+    https://stackoverflow.com/questions/13533763/powershell-mandatory-parameter-depends-on-another-parameter
+    https://stackoverflow.com/questions/13533763/powershell-mandatory-parameter-depends-on-another-parameter
+#>
+
+Param( 
+    [Parameter(Mandatory, ParameterSetName = 'Set', Position = 0)][string[]]$Set,
+    [Parameter(Mandatory, ParameterSetName = 'Set', Position = 1)][string[]]$Value,
+    [Parameter(Mandatory, ParameterSetName = 'Show', Position = 0)][string[]]$Show
 )
 
-function Get-AuditpolSettings {
+function Get-AuditpolSettings($Show) {
     $Auditpol = auditpol /get /category:* 
     $AuditpolSettings = @()
 
     $Auditpol | 
     Select-String 'Success' |
     foreach {
-        $CategoryName = ($_ -split 'Success')[0].Trim()
-        $CategorySetting = 'Success ' + ($_ -split 'Success')[1].Trim()
+        $Name = ($_ -split 'Success')[0].Trim()
+        $Setting = 'Success ' + ($_ -split 'Success')[1].Trim()
         $Category = New-Object psobject
-        Add-Member -InputObject $Category -MemberType NoteProperty -Name Category -Value $CategoryName
-        Add-Member -InputObject $Category -MemberType NoteProperty -Name Setting -Value $CategorySetting
+        Add-Member -InputObject $Category -MemberType NoteProperty -Name Category -Value $Name
+        Add-Member -InputObject $Category -MemberType NoteProperty -Name Setting -Value $Setting
         $AuditpolSettings = $AuditpolSettings + $Category
     }
 
     $Auditpol | 
     Select-String 'No Auditing' |
     foreach {
-        $CategoryName = ($_ -split 'No Auditing')[0].Trim()
-        $CategorySetting = 'No Auditing ' + ($_ -split 'No Auditing')[1].Trim()
+        $Name = ($_ -split 'No Auditing')[0].Trim()
+        $Setting = 'No Auditing ' + ($_ -split 'No Auditing')[1].Trim()
         $Category = New-Object psobject
-        Add-Member -InputObject $Category -MemberType NoteProperty -Name Category -Value $CategoryName
-        Add-Member -InputObject $Category -MemberType NoteProperty -Name Setting -Value $CategorySetting
+        Add-Member -InputObject $Category -MemberType NoteProperty -Name Category -Value $Name
+        Add-Member -InputObject $Category -MemberType NoteProperty -Name Setting -Value $Setting
         $AuditpolSettings = $AuditpolSettings + $Category
     }
 
-    $ProcessCreationCategory = New-Object psobject
-    Add-Member -InputObject $ProcessCreationCategory -MemberType NoteProperty -Name Category -Value 'Process Creation (Command Line)'
-    $RegistryKey = 'Registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\Audit\'
-    $RegistryKeySetting = 'ProcessCreationIncludeCmdLine_Enabled'
-    $RegistryKeyValue = (Get-ItemProperty $RegistryKey).ProcessCreationIncludeCmdLine_Enabled
-    if ($RegistryKeyValue -eq 1) {
-        Add-Member -InputObject $ProcessCreationCategory -MemberType NoteProperty -Name Setting -Value 'Success' 
+    $CmdLineCategory = New-Object psobject
+    Add-Member -InputObject $CmdLineCategory -MemberType NoteProperty -Name Category -Value 'Process Creation (Command Line)'
+    $Key = 'Registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\Audit\'
+    $Setting = 'ProcessCreationIncludeCmdLine_Enabled'
+    $Value = (Get-ItemProperty $Key).ProcessCreationIncludeCmdLine_Enabled
+    if ($Value -eq 1) {
+        Add-Member -InputObject $CmdLineCategory -MemberType NoteProperty -Name Setting -Value 'Success' 
     } else { 
-        Add-Member -InputObject $ProcessCreationCategory -MemberType NoteProperty -Name Setting -Value 'No Auditing'
+        Add-Member -InputObject $CmdLineCategory -MemberType NoteProperty -Name Setting -Value 'No Auditing'
     }
-    $AuditpolSettings = $AuditpolSettings + $ProcessCreationCategory
+    $AuditpolSettings = $AuditpolSettings + $CmdLineCategory
     
-    return $AuditpolSettings | Sort-Object -Property Setting -Descending
+    if ($Show) {
+        if ($Show -like 'all') {
+            return $AuditpolSettings | Sort-Object -Property Setting -Descending
+        } else {
+            if ($Show -contains 'firewall') {
+                $Show = $Show -replace 'firewall','filter'
+            }
+
+            $Show | 
+            ForEach-Object {
+                $Category = $_
+                if ($AuditpolSettings.Category -like $('*' + $Category + '*')) { 
+                    return $AuditpolSettings | Where-Object { $_.Category -like $('*' + $Category + '*') }
+                } 
+            } | Sort-Object -Property Setting -Descending 
+        }
+    }
 }
 
-if ($CommandLine) { Get-AuditpolSettings | Where-Object { $_.Category -like '*Command Line*'} }
-if ($FileShare) { Get-AuditpolSettings | Where-Object { $_.Category -like '*File Share*'} }
-if ($FileSystem) { Get-AuditpolSettings | Where-Object { $_.Category -like '*File System*' } }
-if ($Firewall) { Get-AuditpolSettings | Where-Object { $_.Category -like '*Filtering Platform*' } }
-if ($Logon) { Get-AuditpolSettings | Where-Object { $_.Category -like '*Logon*' } }
-if ($ProcessCreation) { Get-AuditpolSettings | Where-Object { $_.Category -like '*Process Creation*' } }
-if ($Registry) { Get-AuditpolSettings | Where-Object { $_.Category -like '*Registry*' } }
+function Set-AuditpolSettings($Set, $Value) {
 
-<# REFERENCES
-https://stackoverflow.com/questions/5648931/test-if-registry-value-exists
-#>
+}
+
+if ($Show) {
+    Get-AuditpolSettings $Show
+} elseif ($Set -and $Value) {
+    Set-AuditpolSettings $Set $Value
+}
