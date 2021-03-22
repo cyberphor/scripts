844e4b1c261963bfebc24756afc069c9502aeefb
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-03-21 21:06:49 -0600

    Update Disable-StaleComputers.ps1

--- PowerShell/Disable-StaleComputers.ps1
+++ PowerShell/Disable-StaleComputers.ps1
@@ -7,7 +7,9 @@ $SearchBase = Read-Host -Prompt 'Distinguished Name (OU Path in LDAP Format)'
 Get-ADComputer -Filter $Filter -Properties LastLogonDate | 
 foreach {
     if ($_.Enabled) {
-        Set-ADComputer $_.SamAccountName -Description $('Last Login: ' + $_.LastLogonDate)
+        Set-ADComputer $_.SamAccountName -Description $('Last Login - ' + $_.LastLogonDate)
         Disable-ADAccount $_.SamAccountName
     }
 } 
+
+# EXAMPLE OU PATH: OU=Computers,OU=HQ,OU=EvilCorp,DC=vanilla,DC=sky,DC=net
