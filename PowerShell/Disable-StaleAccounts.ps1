c7d5e1ab0664df47d0641f3c531c94f57bb30593
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-03-21 19:33:13 -0600

    Create Disable-StaleAccounts.ps1

---
+++ PowerShell/Disable-StaleAccounts.ps1
@@ -0,0 +1,13 @@
+Import-Module ActiveDirectory
+
+$30_Days_Ago = (Get-Date).AddDays(-30)
+$Filter = { LastLogonDate -le $30_Days_Ago }
+$SearchBase = Read-Host -Prompt 'Distinguished Name (OU Path in LDAP Format)'
+
+Get-ADUser -Filter $Filter -SearchBase $SearchBase -Properties LastLogonDate,Description | 
+foreach {
+    if ($_.Enabled) {
+        Set-ADUser $_.SamAccountName -Description $('Last Login: ' + $_.LastLogonDate)
+        Disable-ADAccount $_.SamAccountName
+    }
+} 
