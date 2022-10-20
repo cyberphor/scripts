4eeb2b447841de05adb887d20cf40e511180e677
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-10-19 20:19:45 -0400

    Moved a repo to this repo

---
+++ PowerShell/random-names/README.md
@@ -0,0 +1,12 @@
+## Random Users
+This repository contains census data, passwords, and PowerShell functions for generating random users. 
+
+### References 
+**PowerShell: Random Users & Weak Passwords (John Hammond)**    
+[https://www.youtube.com/watch?v=66ZD1J-AR2c&t=2832s](https://www.youtube.com/watch?v=66ZD1J-AR2c&t=2832s)
+
+**Frequently Occurring Surnames from Census 1990 – Names Files (United States Census Bureau)**
+[https://www.census.gov/topics/population/genealogy/data/1990_census/1990_census_namefiles.html](https://www.census.gov/topics/population/genealogy/data/1990_census/1990_census_namefiles.html)
+
+**Wordlists (Kali Linux)**  
+[https://gitlab.com/kalilinux/packages/wordlists](https://gitlab.com/kalilinux/packages/wordlists)
---
+++ PowerShell/random-names/Script.ps1
@@ -0,0 +1,51 @@
+﻿function Convert-CensusDataToNameList {
+    Param([Parameter(Mandatory)][string]$InputFile)
+    if (Test-Path $InputFile) {
+        Get-Content $InputFile | 
+        ForEach-Object { 
+            $RandomName = $_.Split(' ')[0].ToLower()
+            (Get-Culture).TextInfo.ToTitleCase($RandomName)
+        }
+    } else {
+        Write-Error "$InputFile not found."
+    }
+}
+
+function Get-Password {
+    Param(
+        [Parameter(Mandatory)][int]$Count,
+        [Parameter(Mandatory)][string]$Passwordlist
+    )
+    Get-Content $Passwordlist -ErrorAction Stop |  
+    Where-Object { 
+        $_.Length -ge 8 -and    # at least 8 char
+        $_ -cmatch "[a-z]" -and # includes at least 1 lowercase char
+        $_ -cmatch "[A-Z]" -and # includes at least 1 upper char
+        $_ -cmatch "[0-9]"      # includes at least 1 number
+    } | Select-Object -First $Count
+}
+
+function Get-RandomUser {
+    Param(
+        [Parameter(Mandatory)][int]$Count,
+        [Parameter(Mandatory)][string]$FirstNameList,
+        [Parameter(Mandatory)][string]$LastNameList,
+        [Parameter(Mandatory)][string]$PasswordList
+    )
+    $FirstNames = Get-Content $FirstNameList | Get-Random -Count $Count
+    $LastNames = Get-Content $LastNameList | Get-Random -Count $Count
+    $Passwords = Get-Content $PasswordList | Get-Random -Count $Count
+    for ($i = 0; $i -lt $Count; $i++) {
+        $FirstName = $FirstNames[$i]
+        $LastName = $LastNames[$i]
+        $Password = $Passwords[$i]
+        $Properties = [ordered]@{
+            "GivenName" = $FirstName
+            "Surname" = $LastName
+            "Name" = "$LastName, $FirstName"
+            "SamAccountName" = ($FirstName + "." + $LastName).ToLower()
+            "Password" = $Password
+        }
+        New-Object -TypeName psobject -Property $Properties
+    }
+}
