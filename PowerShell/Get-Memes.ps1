fc8f4499e0270fc023ab037536117c123a999079
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-11-28 05:58:21 -0800

    Create Get-Memes.ps1

---
+++ PowerShell/Get-Memes.ps1
@@ -0,0 +1,21 @@
+﻿
+"https://ifunny.co",
+"https://ifunny.co/page2",
+"https://ifunny.co/page3",
+"https://ifunny.co/page4",
+"https://ifunny.co/page5" |
+ForEach-Object {
+    $Page = $_
+    (Invoke-WebRequest -Uri $Page -UseBasicParsing).Links.Href | 
+    Select-String -Pattern "ifunny.co/picture" |
+    ForEach-Object {
+        $MemeURL = $_.ToString()
+        (Invoke-WebRequest -Uri $MemeURL).Images |
+        Select-Object -ExpandProperty src |
+        Select-String -Pattern "img.ifunny.co" |
+        ForEach-Object {
+            $Meme = $_.ToString()
+            Start-BitsTransfer -Source $Meme -Destination $PWD
+        }
+    }
+}
